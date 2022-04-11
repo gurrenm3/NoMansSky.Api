6ea93374edf8309e1129b8f2be0ed5fc270fc5f0
Thomas Margraff <thomaspmar@protonmail.com>
2022-04-11 14:35:23 -0700

    Removed testing code

--- NoMansSky.Api/Mod.cs
+++ NoMansSky.Api/Mod.cs
@@ -11,16 +11,16 @@ namespace NoMansSky.Api
     {
         public unsafe Mod(IModConfig _config, IReloadedHooks _hooks, IModLogger _logger) : base(_config, _hooks, _logger)
         {
-            Game.OnMainMenu += () => Logger.WriteLine("Reached Main Menu");
+            /*Game.OnMainMenu += () => Logger.WriteLine("Reached Main Menu");
             Game.OnGameJoined += () => Logger.WriteLine("Game Joined");
 
             Player.Quicksilver.OnValueChanged.Prefix += (newVal) =>
             {
                 Logger.WriteLine($"Quicksilver is changing to {newVal}. Setting it to 1234567 instead");
                 newVal.value = 1234567;
-            };
+            };*/
         }
-        
+
         public override void Update()
         {
             if (Keyboard.IsPressed(Key.UpArrow))
